69baab9e1d90cca92f03d422cc93178f09f45eb8
Huy Pham <huyphamily@gmail.com>
2014-06-27 12:32:48 -0700

    started on every, but will finish later

--- src/underbar.js
+++ src/underbar.js
@@ -214,6 +214,11 @@ var _ = {};
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(alwayFound, item) {
+      if(!alwayFound) {
+        return false;
+      }
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
